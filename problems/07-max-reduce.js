3851e9617e3ef84067c9a35b65ab016c168b24cf
robweekes <weekes.robert@gmail.com>
2024-03-30 15:58:14 -0700

    deleted 7 and 8

--- problems/07-max-reduce.js
+++ problems/07-max-reduce.js
@@ -14,18 +14,6 @@ console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 
 */
 
-let maxWithReduce = function(nums) {
-  const maxNums = nums.reduce((acc, current) => {
-    if(current > acc) acc = current;
-    return acc;
-  })
-    return maxNums;
-};
-
-
-
-console.log(maxWithReduce([4, 6, 3, 5, 42, 4])); // 42
-console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
