a2f9c1c1e4f157506829c2f3fa3f9d651e21890f
admin <admin@admins-iMac.attlocal.net>
2024-03-30 15:12:31 -0700

    problem 07

--- problems/07-max-reduce.js
+++ problems/07-max-reduce.js
@@ -13,11 +13,19 @@ console.log(maxWithReduce([4, 6, 3, 5, 42, 4])); // 42
 console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 
 */
-
+const initial = 0;
 let maxWithReduce = function(nums) {
-    // Your code here 
+  const maxNums = nums.reduce((acc, current) => {
+    if(current > acc) acc = current;
+    return acc;
+  }, 0)
+    return maxNums;
 };
 
+
+
+console.log(maxWithReduce([4, 6, 3, 5, 42, 4])); // 42
+console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
