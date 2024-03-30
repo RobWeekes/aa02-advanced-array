cdb83a394889a047523c49b32a3d538300571dd8
robweekes <weekes.robert@gmail.com>
2024-03-30 14:40:18 -0700

    problem 06

--- problems/06-sum-reduce.js
+++ problems/06-sum-reduce.js
@@ -11,10 +11,35 @@ console.log(sumWithReduce([10, 3, 5, 2])); // 20
 console.log(sumWithReduce([])); // 0
 
 */
+const initial = 0;
 
 let sumWithReduce = function(nums) {
-  // Your code here 
+    const math = nums.reduce(
+      (accumulator, current) => accumulator + current,
+    initial,
+    )
+    console.log(math)
+    return math;
 };
+
+console.log(sumWithReduce([10, 3, 5, 2])); // 20
+console.log(sumWithReduce([])); // 0
+
+
+/// from MDN ---
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
