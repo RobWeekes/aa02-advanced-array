6392b49010a2d73299b4aea9065ffa75886ca420
robweekes <weekes.robert@gmail.com>
2024-03-30 13:51:40 -0700

    problem 04

--- problems/04-remove-e-words-filter.js
+++ problems/04-remove-e-words-filter.js
@@ -12,9 +12,27 @@ console.log(removeEWords('Enter the building')); // 'building'
 
 */
 
+
 let removeEWords = function(sentence) {
-    // Your code here 
+    const wordsARr = sentence.split(' ');
+    console.log(wordsARr);
+    const str = wordsARr.filter((wordsARr) => !wordsARr.includes('e'));
+    console.log(str);
+    return str.join(' ');
 };
+
+console.log(removeEWords('What time is it everyone?')); // 'What is it'
+console.log(removeEWords('Enter the building')); // 'building'
+
+// The filter() method of Array instances creates a shallow copy of a portion of a given
+// array, filtered down to just the elements from the given array that pass the test
+// implemented by the provided function.
+
+const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+const result = words.filter((word) => word.length > 6);
+console.log(result);
+// Expected output: Array ["exuberant", "destruction", "present"]
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
